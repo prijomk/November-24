52068250bd14e2e7d7543372b122dc25f8055e37
prijomk <kprijom@gmail.com>
2023-11-24 22:17:55 +0545

    Question2

---
+++ Question2.java
@@ -0,0 +1,21 @@
+//WAP to check if the user entered string is palindrome or not
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter a string: ");
+        String string = sc.nextLine();
+
+        if (checkpalin(string)) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
+        }
+    }
+
+    static boolean checkpalin(String str) {
+        StringBuilder reversedString = new StringBuilder(str).reverse();
+        return str.equalsIgnoreCase(reversedString.toString());
+    }
+}
